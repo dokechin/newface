cc7111b6d814850fa2976bce00564e6fe1b1b93b
Tetsuya Tatsumi <f7deb9877cyz28@gmail.com>
2024-07-01 16:51:49 +0900

    Create Shohin.java

---
+++ Shohin.java
@@ -0,0 +1,31 @@
+package com.metro.dao;
+
+public class Shohin {
+
+    private String syohin_code;
+    private String syohin_name;
+
+    public Shohin(String line) {
+        if (line == null) {
+            syohin_code = "ZZZZZZZ";
+        } else {
+            String[] lines = line.split(",");
+            syohin_code = lines[0];
+            syohin_name = lines[1];
+        }
+    }
+    
+    public String getSyohin_code() {
+        return syohin_code;
+    }
+    public void setSyohin_code(String syohin_code) {
+        this.syohin_code = syohin_code;
+    }
+    public String getSyohin_name() {
+        return syohin_name;
+    }
+    public void setSyohin_name(String syohin_name) {
+        this.syohin_name = syohin_name;
+    }
+ 
+}

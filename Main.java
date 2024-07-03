5256a3806c089a003ec72a0cf8cfc5864f543971
Tetsuya Tatsumi <f7deb9877cyz28@gmail.com>
2024-07-03 13:10:37 +0900

    Update Main.java
    コメント追加

--- Main.java
+++ Main.java
@@ -17,18 +17,21 @@ public class Main {
               BufferedReader intranReader= Files.newBufferedReader(Paths.get("INTRAN"));
               PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("OTDATA")));){
 
+            // マスタの読み込み　および　マスタオブジェクトShohinへ設定
             String inmastLine = inmastReader.readLine();
             Shohin shohin = new Shohin(inmastLine);            
+            // トランザクションの読み込み　および　トランザクションオブジェクトOrderへ設定
             String intranLine = intranReader.readLine();
             Order order = new Order(intranLine);
 
+            // マスタおよびトランザクションが最終行に到達するまでループする
             while(!shohin.getSyohin_code().equals("ZZZZZZZ") || !order.getSyohin_code().equals("ZZZZZZZ")  ) {
                 if ( shohin.getSyohin_code() .compareTo(order.getSyohin_code()) < 0) {
-                    // master only
+                    // マスタにあり、トランザクションにない場合、次のマスタを読み込む
                     inmastLine = inmastReader.readLine();
                     shohin = new Shohin(inmastLine);            
                 } else if (shohin.getSyohin_code() .compareTo(order.getSyohin_code()) == 0) {
-                    // match
+                    // マスタ及びトランザクションにある場合、個数および金額を合計して、出力
                     int sum_kosuu = 0;
                     int sum_kingaku = 0;
                     String out_hanbai_ymd = order.getHanbai_ymd();
@@ -41,7 +44,7 @@ public class Main {
                     pw.format("%s,%s,%06d,%010d\r\n", shohin.getSyohin_name(),out_hanbai_ymd,sum_kosuu,sum_kingaku);
                     
                 } else {
-                    // trans only
+                    // トランザクションにのみある場合、次のトランザクションを読み込む
                     System.out.println(String.format("TRAN ONLY %s", order.getSyohin_code()));
                     intranLine = intranReader.readLine();
                     order = new Order(intranLine);
